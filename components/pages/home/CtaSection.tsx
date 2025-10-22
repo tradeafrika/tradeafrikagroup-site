d30381dad2a5f69c0123430afe81b215b5a8b7cb
simpletools101 <simpletools@hotmail.com>
2025-10-22 17:36:23 +0300

    sioo

--- components/pages/home/CtaSection.tsx
+++ components/pages/home/CtaSection.tsx
@@ -4,19 +4,21 @@ const CTASection: React.FC = () => {
     return (
         <section className="py-16 px-4 bg-white">
             <div className="max-w-6xl mx-auto">
-                <div className="bg-[#067E48] rounded-3xl px-8 py-16 md:py-24 text-center ">
+                <div className="bg-[#067E48] rounded-3xl space-y-8 px-8 py-16 md:py-24 text-center ">
                     {/* Heading */}
-                    <span className="text-2xl md:text-4xl lg:text-6xl  text-white text-[56px] leading-[1.2] font-bold mb-6">
-                        Join Trade Afrika Group
-                        <br />
-                        Today
-                    </span>
+                    <div className='space-y-6'>
+                        <span className="text-2xl md:text-4xl lg:text-6xl  text-white text-[56px] leading-[1.2] font-bold ">
+                            Join Trade Afrika Group
+                            <br />
+                            Today
+                        </span>
 
-                    {/* Subtitle */}
-                    <p className="text-[16px] leading-[1.5]  md:text-xl text-green-50 mb-10 max-w-2xl mx-auto">
-                        Be part of Africa's fastest-growing digital trade ecosystem. Together we can make intra-African
-                        trade simpler, smarter and more inclusive.
-                    </p>
+                        {/* Subtitle */}
+                        <p className="text-[16px] leading-[1.5]  md:text-xl text-green-50  max-w-2xl mx-auto">
+                            Be part of Africa's fastest-growing digital trade ecosystem. Together we can make
+                            intra-African trade simpler, smarter and more inclusive.
+                        </p>
+                    </div>
 
                     {/* CTA Buttons */}
                     <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
